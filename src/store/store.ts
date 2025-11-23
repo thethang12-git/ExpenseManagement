25bf7d74f2e62e57e34ab7a38ac64a553bde3dc5
nduy2802 <duykk108@gmail.com>
2025-11-23 16:18:52 +0700

    sua lai dropdownmenuavatar

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,12 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "./slices/counterSlice";
 import userReducer from "./slices/user"
-import popupReducer from "./slices/popUp";
+
 export const store = configureStore({
     reducer: {
-        counter: counterReducer,
         user: userReducer,
-        // popup: popupReducer,
     },
 });
 
