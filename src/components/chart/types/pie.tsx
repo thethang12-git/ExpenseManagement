b4b7b5f5cf9a8009b5971736be8b0fd532113c69
thethang12-git <thethang.18.12@gmail.com>
2025-11-24 19:48:05 +0700

    feature/chart updated pie chart done

---
+++ src/components/chart/types/pie.tsx
@@ -0,0 +1,106 @@
+"use client"
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Pie } from 'react-chartjs-2';
+import UserService from "@/src/service/dataService";
+import { useEffect, useState } from "react";
+import { useSelector } from 'react-redux';
+import Switch from '@mui/material/Switch';
+import CloseButton from 'react-bootstrap/CloseButton';
+export default function PieChart({ open, onClose,setSwitch,switchState }: { open: boolean, onClose: () => void, setSwitch: React.Dispatch<React.SetStateAction<boolean>>, switchState: boolean }) {
+ChartJS.register(ArcElement, Tooltip, Legend);
+    const [animation,setAnimation] = useState<boolean>(false);
+    const [Categories, setCategories] = useState<any>([]);
+    const transactions = useSelector((state: any) => state.transactions.list);
+    useEffect(() => {
+        if(open) {setTimeout(() => setAnimation(true), 100);}
+        else {setAnimation(false);}
+    }, [open]);
+    useEffect(() => {
+        const getId = JSON.parse(localStorage.getItem("userId")||"null");
+        if(getId) {
+            UserService.getCategories(getId)
+            .then((res) => {
+                const newCategories = res.data.map((cat:any) => ({
+                    name: cat.name,
+                    value: 0,
+                    id: cat.id
+                }));
+                transactions.forEach((item: any) => {
+                    const index = newCategories.findIndex((c: any) => Number(c.id) === Number(item.categoryId));
+                    if (index !== -1) {
+                    newCategories[index].value += item.money;
+                }
+                });
+            setCategories(newCategories);
+            });
+        }
+    }, []);
+    useEffect(() => {
+        console.log("Categories updated:" ,Categories,transactions);
+    },
+        [Categories]); 
+
+    if (!open) return null;
+
+    const handleClose = () => {
+        setAnimation(false);
+        setTimeout(() => onClose(), 300);
+    }
+const data = {
+  labels: Categories.map((itm: any) => itm.name),
+  datasets: [
+    {
+      label: Categories.map((itm: any) => itm.name),
+      data: Categories.map((itm: any) => itm.value),
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)',
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)',
+      ],
+      borderWidth: 1,
+    },
+  ],
+};
+  return (
+<div
+    
+    style={{opacity: animation ? '1' : '0', transition: 'all 0.3s ease-in-out'}}
+    className="fixed inset-0 bg-black/60 backdrop-blur-sm flex items-center justify-center z-50"
+    onClick={handleClose}
+>
+    <div
+        style={{ transform: animation ? 'scale(1)' : 'scale(0)', transition: 'all 0.4s ease-in-out'}}
+        className="bg-white rounded-xl shadow-xl p-6 w-[90%] max-w-3xl h-[80vh] flex flex-col"
+        onClick={(e) => e.stopPropagation()}
+    >
+        {/* Header */}
+        <div className="flex justify-between items-center mb-4">
+            <h2 className="text-xl font-semibold">Statistics</h2>
+            <div>
+                {switchState ? <span style={{fontWeight:'500'}}>Pie Chart</span> : <span style={{fontWeight:'500'}}>Vertical Chart</span>}
+                <Switch
+                    checked={switchState}
+                    onChange={(e) => setSwitch(e.target.checked)}
+                />
+                <CloseButton onClick={handleClose} />
+            </div>
+        </div>
+        {/* Chart */}
+        <div className="flex-1 flex items-center justify-center">
+            <Pie data={data} />
+        </div>
+    </div>
+</div>
+    )
+}
---
+++ src/components/chart/types/vertical.tsx
@@ -0,0 +1,137 @@
+"use client";
+import UserService from "@/src/service/dataService";
+import Switch from "@mui/material/Switch";
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend,
+} from 'chart.js';
+import { useEffect, useState } from "react";
+import CloseButton from "react-bootstrap/esm/CloseButton";
+import { Bar } from "react-chartjs-2";
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+);
+
+export default function VerticalChart({ open, onClose,setSwitch,switchState }: { open: boolean, onClose: () => void, setSwitch: React.Dispatch<React.SetStateAction<boolean>>, switchState: boolean }) {
+    const [animation,setAnimation] = useState<boolean>(false);
+    const [monthlyData, setMonthlyData] = useState<any>([]);
+    const [maxValue, setMaxValue] = useState<number>(0);
+    const [minValue, setMinValue] = useState<number>(0);
+    let localMax = 0;
+    let localMin = 0;
+    useEffect(() => {
+        if(open) {setTimeout(() => setAnimation(true), 100);}
+        else {setAnimation(false);}
+    }, [open]);
+    useEffect(() => {
+        const getId = JSON.parse(localStorage.getItem("userId")||"null");
+        if(getId) {
+            for(let month=0; month<12; month++){
+                UserService.getTransactionsByMonth(getId,month+1,new Date().getFullYear())
+                    .then((res) => {
+                        let inflow = 0;
+                        let outflow = 0;
+                        res.forEach((curr:any) => {
+                            if(curr.money > 0) {
+                                inflow += curr.money;
+                                if(inflow > localMax) {localMax = inflow;}
+                            }
+                            else {
+                                outflow += curr.money;
+                                if(outflow < localMin) {localMin = outflow;}
+                            }
+                        });
+                        setMonthlyData(
+                            (prev: any) => {
+                                const newData = [...prev];
+                                newData[month] = { inflow, outflow };
+                                return newData;
+                            }
+                        );
+                        setMaxValue(prev => Math.max(prev, localMax));
+                        setMinValue(prev => Math.min(prev, localMin));
+                    });                        
+                }
+            }}, []);    
+            
+    if (!open) return null;
+
+    const handleClose = () => {
+        setAnimation(false);
+        setTimeout(() => onClose(), 300);
+    }
+    const labels = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July','Aug', 'Sept', 'Oct', "Nov", "Dec"];
+    const data = {
+        labels,
+        datasets: [
+            {
+                label: 'Inflow',
+                data: monthlyData.map((itm: any) => itm?.inflow || 0),
+                backgroundColor: 'rgba(53, 162, 235, 0.5)',
+            },
+            {
+                label: 'Outflow',
+                data: monthlyData.map((itm: any) => itm?.outflow || 0),
+                backgroundColor: 'rgba(255, 99, 132, 0.5)',
+            },
+        ],
+    } 
+    const options = {
+        scales: {
+            y: {
+                min: -Math.abs(minValue),
+                max: maxValue,
+            }
+        },
+        responsive: true,
+        plugins: {
+            legend: { position: 'bottom' as const},
+            title: {
+                display: true,
+                text: 'Vertical Bar Chart',
+                font: { size: 18 }
+            },
+        },
+    };
+    return (
+        <div
+            style={{opacity: animation ? '1' : '0', transition: 'all 0.3s ease-in-out'}}
+            className="fixed inset-0 bg-black/60 backdrop-blur-sm flex items-center justify-center z-50"
+            onClick={handleClose}
+        >
+            <div
+                style={{ transform: animation ? 'scale(1)' : 'scale(0)', transition: 'all 0.4s ease-in-out'}}
+                className="bg-white rounded-xl shadow-xl p-6 w-[90%] max-w-3xl h-[80vh] flex flex-col"
+                onClick={(e) => e.stopPropagation()}
+            >
+                {/* Header */}
+                <div className="flex justify-between items-center mb-4">
+                    <h2 className="text-xl font-semibold">Statistics</h2>
+                    <div>
+                        {switchState ? <span style={{fontWeight:'500'}}>Pie Chart</span> : <span style={{fontWeight:'500'}}>Vertical Chart</span>}
+                        <Switch
+                            checked={switchState}
+                            onChange={(e) => setSwitch(e.target.checked)}/>
+                    <CloseButton onClick={handleClose} />
+            </div>
+                </div>
+
+                {/* Chart */}
+                <div className="flex-1">
+                    <Bar options={options}  data={data} />
+                </div>
+            </div>
+        </div>
+    );
+}
+
